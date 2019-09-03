bcf0fb71fda056ec717f7cfb324660c2ab47fc6d
Hao Tran <account@haothitran.com>
2019-09-02 21:00:31 -0400

    Convert double quotes to single quotes

--- Config.lua
+++ Config.lua
@@ -10,21 +10,21 @@ C.NumberAddOns = 40                 -- number of addons to show in stats tooltip
 C.Clock24 = true                    -- show time in 24 hour format
 
 C.Font = {
-  ["Family"] = STANDARD_TEXT_FONT,  -- font family
-  ["Size"] = 12,                    -- font size
-  ["Style"] = "OUTLINE",            -- font outline
-  ["Align"] = "CENTER",             -- text justification
+  ['Family'] = STANDARD_TEXT_FONT,  -- font family
+  ['Size'] = 12,                    -- font size
+  ['Style'] = 'OUTLINE',            -- font outline
+  ['Align'] = 'CENTER',             -- text justification
 }
 
 C.Size = {
-  ["Width"] = 240,                  -- frame width
-  ["Height"] = 24,                  -- frame height
+  ['Width'] = 240,                  -- frame width
+  ['Height'] = 24,                  -- frame height
 }
 
 C.Position = {
-  ["Point"] = "CENTER",              -- attachment point to parent
-	["RelativeTo"] = "UIParent",       -- parent frame
-	["RelativePoint"] = "CENTER",      -- parent attachment point
-	["XOffset"] = 0,                   -- horizontal offset from parent point
-	["YOffset"] = 0,                   -- vertical offset from parent point
+  ['Point'] = 'CENTER',              -- attachment point to parent
+  ['RelativeTo'] = 'UIParent',       -- parent frame
+  ['RelativePoint'] = 'CENTER',      -- parent attachment point
+  ['XOffset'] = 0,                   -- horizontal offset from parent point
+  ['YOffset'] = 0,                   -- vertical offset from parent point
 }
--- Stats.lua
+++ Stats.lua
@@ -5,96 +5,96 @@ local C = ns.C
 -- // STATS
 --------------------------------------------------------------------------------
 
-local loader = CreateFrame("Frame")
-loader:RegisterEvent("ADDON_LOADED")
-loader:SetScript("OnEvent", function(self, addon)
-	if addon ~= KlazStats then
-		local function initDB(db, defaults)
-			if type(db) ~= "table" then db = {} end
-			if type(defaults) ~= "table" then return db end
-			for k, v in pairs(defaults) do
-				if type(v) == "table" then
-					db[k] = initDB(db[k], v)
-				elseif type(v) ~= type(db[k]) then
-					db[k] = v
-				end
-			end
-		return db
-	end
+local loader = CreateFrame('Frame')
+loader:RegisterEvent('ADDON_LOADED')
+loader:SetScript('OnEvent', function(self, addon)
+  if addon ~= KlazStats then
+    local function initDB(db, defaults)
+      if type(db) ~= 'table' then db = {} end
+      if type(defaults) ~= 'table' then return db end
+      for k, v in pairs(defaults) do
+        if type(v) == 'table' then
+          db[k] = initDB(db[k], v)
+        elseif type(v) ~= type(db[k]) then
+          db[k] = v
+        end
+      end
+    return db
+  end
 
-		KlazStatsDB = initDB(KlazStatsDB, C.Position)
-		C.UserPlaced = KlazStatsDB
-		self:UnregisterEvent("ADDON_LOADED")
-	end
+    KlazStatsDB = initDB(KlazStatsDB, C.Position)
+    C.UserPlaced = KlazStatsDB
+    self:UnregisterEvent('ADDON_LOADED')
+  end
 end)
 
 --------------------------------------------------------------------------------
 -- // ANCHOR FRAME
 --------------------------------------------------------------------------------
 
-local anchor = CreateFrame("Frame", "KlazStatsAnchor", UIParent)
+local anchor = CreateFrame('Frame', 'KlazStatsAnchor', UIParent)
 anchor:SetSize(C.Size.Width, C.Size.Height)
 anchor:SetBackdrop({bgFile=[[Interface\MINIMAP\TooltipBackdrop-Background]]})
-anchor:SetFrameStrata("HIGH")
+anchor:SetFrameStrata('HIGH')
 anchor:SetMovable(true)
 anchor:SetClampedToScreen(true)
 anchor:EnableMouse(true)
 anchor:SetUserPlaced(true)
-anchor:RegisterForDrag("LeftButton")
-anchor:RegisterEvent("PLAYER_LOGIN")
+anchor:RegisterForDrag('LeftButton')
+anchor:RegisterEvent('PLAYER_LOGIN')
 anchor:Hide()
 
-anchor.text = anchor:CreateFontString(nil, "OVERLAY")
+anchor.text = anchor:CreateFontString(nil, 'OVERLAY')
 anchor.text:SetAllPoints(anchor)
 anchor.text:SetFont(C.Font.Family, C.Font.Size, C.Font.Style)
 anchor.text:SetShadowOffset(0, 0)
-anchor.text:SetText("KlazStatsAnchor")
+anchor.text:SetText('KlazStatsAnchor')
 
-anchor:SetScript("OnEvent", function(self, event, arg1)
-	if event == "PLAYER_LOGIN" then
-		self:ClearAllPoints()
-		self:SetPoint(
-		C.UserPlaced.Point,
-		C.UserPlaced.RelativeTo,
-		C.UserPlaced.RelativePoint,
-		C.UserPlaced.XOffset,
-		C.UserPlaced.YOffset)
-	end
+anchor:SetScript('OnEvent', function(self, event, arg1)
+  if event == 'PLAYER_LOGIN' then
+    self:ClearAllPoints()
+    self:SetPoint(
+    C.UserPlaced.Point,
+    C.UserPlaced.RelativeTo,
+    C.UserPlaced.RelativePoint,
+    C.UserPlaced.XOffset,
+    C.UserPlaced.YOffset)
+  end
 end)
 
-anchor:SetScript("OnDragStart", function(self)
-	self:StartMoving()
+anchor:SetScript('OnDragStart', function(self)
+  self:StartMoving()
 end)
 
-anchor:SetScript("OnDragStop", function(self)
-	self:StopMovingOrSizing()
-	self:SetUserPlaced(false)
+anchor:SetScript('OnDragStop', function(self)
+  self:StopMovingOrSizing()
+  self:SetUserPlaced(false)
 
-	point, relativeTo, relativePoint, xOffset, yOffset = self:GetPoint(1)
-		if relativeTo then
-			relativeTo = relativeTo:GetName();
-		else
-			relativeTo = self:GetParent():GetName();
-		end
+  point, relativeTo, relativePoint, xOffset, yOffset = self:GetPoint(1)
+    if relativeTo then
+      relativeTo = relativeTo:GetName();
+    else
+      relativeTo = self:GetParent():GetName();
+    end
 
-	C.UserPlaced.Point = point
-	C.UserPlaced.RelativeTo = relativeTo
-	C.UserPlaced.RelativePoint = relativePoint
-	C.UserPlaced.XOffset = xOffset
-	C.UserPlaced.YOffset = yOffset
+  C.UserPlaced.Point = point
+  C.UserPlaced.RelativeTo = relativeTo
+  C.UserPlaced.RelativePoint = relativePoint
+  C.UserPlaced.XOffset = xOffset
+  C.UserPlaced.YOffset = yOffset
 end)
 
 --------------------------------------------------------------------------------
 -- // STATS FRAME
 --------------------------------------------------------------------------------
 
-local class = RAID_CLASS_COLORS[select(2, UnitClass("player"))]
+local class = RAID_CLASS_COLORS[select(2, UnitClass('player'))]
 
-local stats = CreateFrame("Frame", "KlazStats", UIParent)
+local stats = CreateFrame('Frame', 'KlazStats', UIParent)
 stats:SetAllPoints(anchor)
 stats:SetSize(C.Size.Width, C.Size.Height)
 
-stats.text = stats:CreateFontString(nil, "BACKGROUND")
+stats.text = stats:CreateFontString(nil, 'BACKGROUND')
 stats.text:SetPoint(C.Font.Align, stats)
 stats.text:SetJustifyH(C.Font.Align)
 stats.text:SetFont(C.Font.Family, C.Font.Size, C.Font.Style)
@@ -105,15 +105,15 @@ stats.text:SetTextColor(class.r, class.g, class.b)
 --------------------------------------------------------------------------------
 
 local function memFormat(number)
-	if number > 1024 then
-		return string.format("%.2f mb", (number / 1024))
-	else
-		return string.format("%.1f kb", floor(number))
-	end
+  if number > 1024 then
+    return string.format('%.2f mb', (number / 1024))
+  else
+    return string.format('%.1f kb', floor(number))
+  end
 end
 
 local function compareMemory(a, b)
-	return a.memory > b.memory
+  return a.memory > b.memory
 end
 
 --------------------------------------------------------------------------------
@@ -121,32 +121,32 @@ end
 --------------------------------------------------------------------------------
 
 local function getFPS()
-	return "|cffffffff"..floor(GetFramerate()).."|r fps"
+  return '|cffffffff'..floor(GetFramerate())..'|r fps'
 end
 
 local function getLatencyRaw()
-	return select(3, GetNetStats())
+  return select(3, GetNetStats())
 end
 
 local function getLatency()
-	return "|cffffffff"..getLatencyRaw().."|r ms"
+  return '|cffffffff'..getLatencyRaw()..'|r ms'
 end
 
 local function getLatencyWorldRaw()
-	return select(4, GetNetStats())
+  return select(4, GetNetStats())
 end
 
 local function getLatencyWorld()
-	return "|cffffffff"..getLatencyWorldRaw().."|r ms"
+  return '|cffffffff'..getLatencyWorldRaw()..'|r ms'
 end
 
 local function getTime()
-	local t
+  local t
   if C.Clock24 == true then
-    t = date("%H:%M")
-    return "|cffffffff"..t.."|r "
+    t = date('%H:%M')
+    return '|cffffffff'..t..'|r '
   else
-    t = date("|cffffffff%I:%M|r %p")
+    t = date('|cffffffff%I:%M|r %p')
     return t
   end
 
@@ -154,71 +154,71 @@ end
 
 local SLOTS = {}
 for _, slot in pairs({
-  "Head",
-  "Shoulder",
-  "Chest",
-  "Waist",
-  "Legs",
-  "Feet",
-  "Wrist",
-  "Hands",
-  "MainHand",
-  "SecondaryHand"
+  'Head',
+  'Shoulder',
+  'Chest',
+  'Waist',
+  'Legs',
+  'Feet',
+  'Wrist',
+  'Hands',
+  'MainHand',
+  'SecondaryHand'
 }) do
-  SLOTS[slot] = GetInventorySlotInfo(slot.."Slot")
+  SLOTS[slot] = GetInventorySlotInfo(slot..'Slot')
 end
 
 local function durPercent(perc)
-	perc = perc > 1 and 1 or perc < 0 and 0 or perc
-	local seg, relperc = math.modf(perc*2)
-	local r1, g1, b1, r2, g2, b2 = select(seg*3+1,1,0,0,1,1,0,1,1,1,0,0,0)
-	local r, g, b = r1+(r2-r1)*relperc, g1+(g2-g1)*relperc, b1+(b2-b1)*relperc
-	return format("|cff%02x%02x%02x", r*255, g*255, b*255), r, g, b
+  perc = perc > 1 and 1 or perc < 0 and 0 or perc
+  local seg, relperc = math.modf(perc*2)
+  local r1, g1, b1, r2, g2, b2 = select(seg*3+1,1,0,0,1,1,0,1,1,1,0,0,0)
+  local r, g, b = r1+(r2-r1)*relperc, g1+(g2-g1)*relperc, b1+(b2-b1)*relperc
+  return format('|cff%02x%02x%02x', r*255, g*255, b*255), r, g, b
 end
 
 local function getDurability()
-	local l = 1
-	for slot, id in pairs(SLOTS) do
-		local d, md = GetInventoryItemDurability(id)
-		if d and md and md ~= 0 then
-			l = math.min(d/md, l)
-		end
-	end
-  return format("%s%d|r dur", durPercent(l), l*100)
+  local l = 1
+  for slot, id in pairs(SLOTS) do
+    local d, md = GetInventoryItemDurability(id)
+    if d and md and md ~= 0 then
+      l = math.min(d/md, l)
+    end
+  end
+  return format('%s%d|r dur', durPercent(l), l*100)
 end
 
 --------------------------------------------------------------------------------
 -- // CLICK GARBAGE COLLECTION
 --------------------------------------------------------------------------------
 
-stats:SetScript("OnMouseDown", function()
+stats:SetScript('OnMouseDown', function()
   UpdateAddOnMemoryUsage()
-	local before = gcinfo()
-	collectgarbage()
-	UpdateAddOnMemoryUsage()
-	local after = gcinfo()
-	print("|cff1994ffCleaned:|r "..memFormat(before-after))
+  local before = gcinfo()
+  collectgarbage()
+  UpdateAddOnMemoryUsage()
+  local after = gcinfo()
+  print('|cff1994ffCleaned:|r '..memFormat(before-after))
 end)
 
 --------------------------------------------------------------------------------
 -- // TOOLTIP
 --------------------------------------------------------------------------------
 
-stats:SetScript("OnEnter", function(self)
-	if IsModifierKeyDown() then return end
+stats:SetScript('OnEnter', function(self)
+  if IsModifierKeyDown() then return end
   if not InCombatLockdown() then
     GameTooltip:ClearLines()
-    GameTooltip:SetOwner(self, "ANCHOR_NONE")
-    GameTooltip:SetPoint("BOTTOM", self, "TOP", 0, 12)
+    GameTooltip:SetOwner(self, 'ANCHOR_NONE')
+    GameTooltip:SetPoint('BOTTOM', self, 'TOP', 0, 12)
 
     local addons, total, nr, name = {}, 0, 0
-    local blizz = collectgarbage("count")
+    local blizz = collectgarbage('count')
     local entry, memory
     UpdateAddOnMemoryUsage()
 
-    GameTooltip:AddLine("Stats", .098, .58, 1)
-    GameTooltip:AddLine(" ")
-    GameTooltip:AddLine(TRACK_QUEST_TOP_SORTING.." "..C.NumberAddOns.." "..ADDONS, class.r, class.g, class.b)
+    GameTooltip:AddLine('Stats', .098, .58, 1)
+    GameTooltip:AddLine(' ')
+    GameTooltip:AddLine(TRACK_QUEST_TOP_SORTING..' '..C.NumberAddOns..' '..ADDONS, class.r, class.g, class.b)
 
     for i = 1, GetNumAddOns(), 1 do
       if GetAddOnMemoryUsage(i) > 0 then
@@ -238,19 +238,19 @@ stats:SetScript("OnEnter", function(self)
       end
     end
 
-    GameTooltip:AddLine(" ")
+    GameTooltip:AddLine(' ')
     GameTooltip:AddDoubleLine(TOTAL, memFormat(total), 1, 1, 1, 1, 1, 1)
-    GameTooltip:AddLine(" ")
+    GameTooltip:AddLine(' ')
     GameTooltip:AddLine(NETWORK_LABEL, class.r, class.g, class.b)
-    GameTooltip:AddDoubleLine(HOME, getLatencyRaw().." ms", 1, 1, 1, 1, 1, 1)
-    GameTooltip:AddDoubleLine(WORLD, getLatencyWorldRaw().." ms", 1, 1, 1, 1, 1, 1)
+    GameTooltip:AddDoubleLine(HOME, getLatencyRaw()..' ms', 1, 1, 1, 1, 1, 1)
+    GameTooltip:AddDoubleLine(WORLD, getLatencyWorldRaw()..' ms', 1, 1, 1, 1, 1, 1)
 
     GameTooltip:Show()
   end
 end)
 
-stats:SetScript("OnLeave", function()
-	GameTooltip:Hide()
+stats:SetScript('OnLeave', function()
+  GameTooltip:Hide()
 end)
 
 --------------------------------------------------------------------------------
@@ -259,21 +259,21 @@ end)
 
 local lastUpdate = 0
 local function updateStats(self,elapsed)
-	lastUpdate = lastUpdate + elapsed
+  lastUpdate = lastUpdate + elapsed
 
-	if lastUpdate > 1 then
-		lastUpdate = 0
-		stats.text:SetText(getFPS().."    "..getLatency().."    "..getDurability().."    "..getTime())
-	end
+  if lastUpdate > 1 then
+    lastUpdate = 0
+    stats.text:SetText(getFPS()..'    '..getLatency()..'    '..getDurability()..'    '..getTime())
+  end
 end
 
-stats:RegisterEvent("PLAYER_LOGIN")
-stats:RegisterEvent("PLAYER_ENTERING_WORLD")
-stats:RegisterEvent("UPDATE_INVENTORY_DURABILITY")
-stats:SetScript("OnEvent", function(self, event)
-	if event == "PLAYER_LOGIN" then
-		self:SetScript("OnUpdate", updateStats)
-	end
+stats:RegisterEvent('PLAYER_LOGIN')
+stats:RegisterEvent('PLAYER_ENTERING_WORLD')
+stats:RegisterEvent('UPDATE_INVENTORY_DURABILITY')
+stats:SetScript('OnEvent', function(self, event)
+  if event == 'PLAYER_LOGIN' then
+    self:SetScript('OnUpdate', updateStats)
+  end
 end)
 
 --------------------------------------------------------------------------------
@@ -281,25 +281,25 @@ end)
 --------------------------------------------------------------------------------
 
 SlashCmdList.KLAZSTATS = function (msg, editbox)
-	if (string.lower(msg) == "reset") then
-		KlazStatsDB = C.Position
-		ReloadUI()
-	elseif (string.lower(msg) == "unlock") then
-		if not anchor:IsShown() then
+  if string.lower(msg) == 'reset' then
+    KlazStatsDB = C.Position
+    ReloadUI()
+  elseif string.lower(msg) == 'unlock' then
+    if not anchor:IsShown() then
       anchor:Show()
-			print("|cff1994ffKlazStats|r |cff00ff00Unlocked.|r")
+      print('|cff1994ffKlazStats|r |cff00ff00Unlocked.|r')
     end
-	elseif (string.lower(msg) == "lock") then
-		anchor:Hide()
-		print("|cff1994ffKlazStats|r |cffff0000Locked.|r")
-	else
-		print("------------------------------------------")
-		print("|cff1994ffKlazStats commands:|r")
-		print("------------------------------------------")
-		print("|cff1994ff/klazstats unlock|r Unlocks frame to be moved.")
-		print("|cff1994ff/klazstats lock|r Locks frame in position.")
-		print("|cff1994ff/klazstats reset|r Resets frame to default position.")
-	end
+  elseif string.lower(msg) == 'lock' then
+    anchor:Hide()
+    print('|cff1994ffKlazStats|r |cffff0000Locked.|r')
+  else
+    print('------------------------------------------')
+    print('|cff1994ffKlazStats commands:|r')
+    print('------------------------------------------')
+    print('|cff1994ff/klazstats unlock|r Unlocks frame to be moved.')
+    print('|cff1994ff/klazstats lock|r Locks frame in position.')
+    print('|cff1994ff/klazstats reset|r Resets frame to default position.')
+  end
 end
-SLASH_KLAZSTATS1 = '/kstats'
-SLASH_KLAZSTATS2 = '/klazstats'
+SLASH_KLAZSTATS1 = '/klazstats'
+SLASH_KLAZSTATS2 = '/kstats'
